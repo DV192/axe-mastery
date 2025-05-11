232c0b97994aa85526eca3cbe9f944c696cf4da1
Varun Patel <varunvarshil111@gmail.com>
2025-05-10 21:26:46 -0400

    Initial scene setup

--- components/home/CanvasRenderer.jsx
+++ components/home/CanvasRenderer.jsx
@@ -1,27 +1,63 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stats } from "@react-three/drei";
-import Experience from "./Experience";
+import { Loader, OrbitControls, PositionalAudio, Stats, useGLTF } from "@react-three/drei";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { Perf } from "r3f-perf";
 
+import Experience from "./Experience";
+
+useGLTF.preload("models/balloon_modified.glb");
+useGLTF.preload("models/Axe Small Applied.glb");
+useGLTF.preload("models/Axe Small.glb");
+useGLTF.preload("models/AncientRuins-v1.glb");
+useGLTF.preload("models/Ancient Ruins target.glb");
+
 const CanvasRenderer = () => {
+  const debug = true;
+
   return (
     <div className="w-full h-full fixed inset-0">
-      <Canvas>
-        <Stats />
-        <Perf />
-        <OrbitControls
-          makeDefault
-          enableZoom={false}
-          enablePan={false}
-        />
+      <Canvas shadows camera={{ position: [-0.1, 0, 0], fov: 50 }}>
+        {debug && (
+          <>
+            <Stats />
+            <Perf />
+            {/* <OrbitControls
+              makeDefault
+              enableZoom={false}
+              enablePan={false}
+            /> */}
+          </>
+        )}
+
+        <color attach="background" args={["#111"]} />
 
         <Suspense fallback={null}>
           <Experience />
         </Suspense>
+
+        <EffectComposer>
+          <Bloom mipmapBlur intensity={0.5} luminanceThreshold={1.5} />
+        </EffectComposer>
+
+        <Preloader />
       </Canvas>
+
+      <Loader />
     </div>
   )
 }
 
 export default CanvasRenderer
+
+export const AUDIOS = {
+  pop: "sfxs/balloon-pop-48030.mp3",
+  impact: "sfxs/cinematic-hit-159487-cut.mp3",
+  throw: "sfxs/axe-slash-1-106748-cut.mp3",
+};
+
+const Preloader = () => {
+  return Object.values(AUDIOS).map((audio) => (
+    <PositionalAudio key={audio} url={audio} loop={false} autoplay={false} />
+  ));
+};
--- components/home/Experience.jsx
+++ components/home/Experience.jsx
@@ -1,11 +1,47 @@
+import { useRef } from "react";
+import { CameraControls, Environment, Grid, PerspectiveCamera } from "@react-three/drei";
+
+import { GradientSky } from "./GradientSky";
 
 const Experience = () => {
+  const controls = useRef();
+
   return (
     <>
-      <group rotation-x={(Math.PI / 180) * 15}>
-        <mesh>
-          <boxGeometry />
-          <meshBasicMaterial />
+      <CameraControls ref={controls} />
+      <GradientSky />
+      <Environment preset="sunset" environmentIntensity={0.3} />
+      <Grid
+        position-y={-10}
+        infiniteGrid
+        sectionColor={"#999"}
+        cellColor={"#555"}
+        fadeStrength={5}
+      />
+      <directionalLight
+        position={[30, 15, 30]}
+        castShadow
+        intensity={1}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-bias={-0.005}
+      >
+        <PerspectiveCamera
+          attach={"shadow-camera"}
+          near={10}
+          far={50}
+          fov={80}
+        />
+      </directionalLight>
+
+      <group>
+        <mesh position-x={10}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial
+            color={"orange"}
+            emissive={"orange"}
+            emissiveIntensity={8}
+          />
         </mesh>
       </group>
     </>
